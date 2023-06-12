63f8dbab82d9fb1f32b2caa5a0c1027401591fb6
Brett Sira <sirawat21@gmail.com>
2023-06-12 22:26:21 +1000

    Created client side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery, gql } from "@apollo/client";
 
-function App() {
-  const [count, setCount] = useState(0)
+const GET_LOCATIONS = gql`
+  query hello {
+  }
+`;
 
+export default function App() {
+  const DisplayLocations = async () => {
+    const { loading, error, data } = await useQuery(GET_LOCATIONS);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+    const result = JSON.stringify(data);
+    return <p>{result}</p>;
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <>
+      {DisplayLocations}
+      </>
+    </div>
+  );
 }
-
-export default App
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+// Initialising client instant
+const host = "localhost:3300";
+const client = new ApolloClient({
+  uri: host,
+  cache: new InMemoryCache(),
+});
+
+// Making client request
+client.query({
+    query: gql`
+      query hello {
+      }
+    `,
+  }).then((result) => console.log(result));
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-)
+  </ApolloProvider>
+);
